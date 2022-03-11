a2c7e1aedaa9eec8f0b192557ca716f0d31416c2
qkrxodud00 <qkrxodud00@gmail.com>
2022-03-11 17:51:39 +0900

    *할인율로 적용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,12 +1,12 @@
 package hello.core.order;
 
 import hello.core.discout.DiscountPolicy;
-import hello.core.discout.FixDiscountPolicy;
+import hello.core.discout.RateDiscountPolicy;
 import hello.core.member.*;
 
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
