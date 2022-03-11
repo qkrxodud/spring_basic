e71b26014f151a81b5ee7e63c0471802c277b930
qkrxodud00 <qkrxodud00@gmail.com>
2022-03-11 14:51:51 +0900

    * 주문과 할인 도메인 개발

---
+++ src/main/java/hello/core/discout/FixDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.core.discout;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        int defaultValue = 0;
+        if (member.getGrade() == Grade.VIP) {
+            defaultValue = discountFixAmount;
+        }
+        return defaultValue;
+    }
+}
