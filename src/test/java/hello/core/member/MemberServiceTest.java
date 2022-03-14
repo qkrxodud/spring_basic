2e3a3634e7299169caf31194a64cd525215d463f
qkrxodud00 <qkrxodud00@gmail.com>
2022-03-14 15:12:08 +0900

    * 테스트 코드  - 테스트 할 때마다 초기화 적용

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,13 +1,22 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl(memberRepository);
+    AppConfig appConfig;
+    MemberService memberService;
     // 문제 점 다른 저장소로 변경할 때 OCP 개방폐쇄의 원칙을 잘 준수한가?
     // DIP를 잘 지키고 있는가?
     // 의존관계가 인터페이스 뿐만 아니라 구현까지 모두 의존하는 문제점이 있음
+
+    @BeforeEach
+    public void beforeEach () {
+        appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
